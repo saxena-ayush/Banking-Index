5dc615de676b825ba1ce98a486e27b77de78150a
Ayush Saxena <78346400+saxena-ayush@users.noreply.github.com>
2022-10-28 01:07:09 +0530

    Banking top clusters website

---
+++ script.js
@@ -0,0 +1,39 @@
+var c0 =  {"0":
+{"Mahbubnagar":28147,"Dhubri":23772,"Sheopur":23720,"Bhind":21025,"Sitamarhi":20749,"Nandurbar":20604,"Garhwa":18372,"Udalguri":17696,"Saharsa":17599,"Bahraich":17097,"Maharajganj":16077,"Chirang":16073,"Jalna":16058,"Bid":15860,
+"Latehar":15804}
+}
+
+var c1 = {"0":{"Medak":30952,"Senapati":28186,"Surguja":24583,"Tikamgarh":22234,"Morena":21605,"West Garo Hills":18380,"Karimnagar":18243,"Dohad":18027,"Purbi Champaran":17893,"Chatra":17382,"Araria":17249,"Hardoi":16638,"Nagaon":16514,"Nawada":16439,"Katihar":16336,"Mainpuri":16109,"Barmer":15877,"Panna":15639,
+"Karbi Anglong":15425}}
+
+var c2 = {"0":{"Dindori":17184,
+"Kanshiram Nagar":16707}}
+
+var c3 ={"0":{"Umaria":16533}}
+
+var c4 ={"0":{"Adilabad":25861,"Alirajpur":21442,"Thoubal":21109,"Budaun":21040,"Sultanpur":20979,"Banka":19196,"Supaul":18123,"Baksa":17594,"Shivpuri":17261,"Sidhi":16822,"Kokrajhar":16739,"Madhubani":16682,"Madhepura":16682,"Parbhani":15966}}
+
+
+    
+
+ 
+
+var cl0 = document.getElementById("clstr0");
+
+cl0.innerHTML =Object.keys(c0[0])[0]
+
+var cl1 = document.getElementById("clstr1");
+
+cl1.innerHTML =Object.keys(c1[0])[0]
+
+var cl2 = document.getElementById("clstr2");
+
+cl2.innerHTML =Object.keys(c2[0])[0]
+
+var cl3 = document.getElementById("clstr3");
+
+cl3.innerHTML =Object.keys(c3[0])[0]
+
+var cl4 = document.getElementById("clstr4");
+
+cl4.innerHTML =Object.keys(c4[0])[0]
